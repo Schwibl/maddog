ecddca9c7b954c40db10a5303cefe70d3d15d210
Bosikova Liubov Sergeevna <gulidova06@mail.ru>
2023-11-29 23:10:31 +0300

    authorize page started

--- maddog/src/App.js
+++ maddog/src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from './panel-logo2x.webp';
+import styles from './App.module.scss';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <section className={styles.login}>
+        <div className={styles.container}>
+            <a href='/' className={styles.imgSide}>
+                <img src={logo} className={styles.img} />
+            </a>
+            <div className={styles.formSide}>
+                <form className={styles.form} action='/login' method='post'>
+                    <h1 className={styles.enter}>ВХОД В СИСТЕМУ</h1>
+                    <div className={styles.input}>
+                        <div className={stylrs.inputWrap}>
+                            <label className={styles.label}>Логин*</label>
+                            <input className={styles.input} type='text' name='username' placeholder='Можете ввести свой логин, телефон или почту...' autocomplete='on' required />
+                        </div>
+                        <div className={styles.inputWrap}></div>
+                            <label className={styles.label}>Пароль*</label>
+                            <input className={styles.input} type='password' name='password' placeholder='Введите свой пароль...' autocomplete='on' required />
+                    </div>
+                        <input className={styles.submit} name='submit' type='submit' value='ВХОД' />
+                </form>
+            </div>
+        </div>
+      </section>
     </div>
   );
 }
