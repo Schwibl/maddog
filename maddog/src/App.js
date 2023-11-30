7c7d7419b5055aa5497bb07595c7c0665e770ecc
Sivchenko-Anna <anya.malakhova.99@mail.ru>
2023-11-30 23:37:39 +1000

    add user context

--- maddog/src/App.js
+++ maddog/src/App.js
@@ -1,23 +1,26 @@
 // import logo from './panel-logo2x.webp';
 // import styles from './App.module.scss';
-import {
-  BrowserRouter,
-  Routes,
-  Route,
-} from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import AuthorizePage from './authorizePage/AuthorizePage';
 import ProjectPage from './projectsPages/ProjectPage';
-
+import UserContext from './UserContext';
 
 function App() {
+  const user = {
+    id: 123,
+    role: 'admin',
+    contacts: 'example@example.com',
+  };
   return (
-    <BrowserRouter className='App'>
-      <Routes>
-          <Route path="/" element={<AuthorizePage />} />
-          <Route path="projects" element={<ProjectPage />} />
+    <UserContext.Provider value={user}>
+      <BrowserRouter className='App'>
+        <Routes>
+          <Route path='/' element={<AuthorizePage />} />
+          <Route path='projects' element={<ProjectPage />} />
           {/* <Route path="contacts" element={<ContactsPage />} /> */}
-      </Routes>
-    </BrowserRouter>
+        </Routes>
+      </BrowserRouter>
+    </UserContext.Provider>
   );
 }
 
