5553f84a7fde1969408d25737ff10c0461bd36ff
Bosikova Liubov Sergeevna <gulidova06@mail.ru>
2023-12-07 23:28:01 +0300

    new fields

--- maddog/src/adminPage/AdminPage.jsx
+++ maddog/src/adminPage/AdminPage.jsx
@@ -1,6 +1,8 @@
 import NavBar from '../components/navbar/NavBar';
-import AdminRow from '../components/admin/AdminRow';
+import AdminTable from '../components/admin/AdminTable';
 import styles from './AdminPage.module.scss';
+import AdminsContext from '../context/AdminsContext';
+import { useState } from 'react';
 
 // Тестовые данные
 const admins = [
@@ -8,44 +10,47 @@ const admins = [
         id: 0,
         login: 'skelorc',
         name: 'Petrov Ivan',
-        role: 'ADMIN'
+        role: 'ADMIN',
+        contact: '89889889888'
     },
     {
         id: 1,
         login: 'philipp',
         name: 'philipp boss',
-        role: 'ADMIN'
+        role: 'ADMIN',
+        contact: '89889889888'
     },
     {
         id: 2,
         login: 'adm',
         name: 'best admin',
-        role: 'ADMIN'
+        role: 'ADMIN',
+        contact: '89889889888'
     }
 ]
 
 export default function AdminPage (props) {
+
     return (
-        <div className={styles.container}>
-            <NavBar />
-            <section className={styles.adminPage}>
-                <h1 className={styles.title}>Администраторы</h1>
-                <div className={styles.currentAdmins}>
-                    <div className={styles.anminsHeader}>
-                        <p>Логин</p>
-                        <p>ФИО</p>
-                        <p>Роль</p>
-                        <div></div>
-                        <div></div>
-                    </div>
-                    <div className={styles.admins}>
-                        {
-                            admins.map(({ id, login, name, role }) => <AdminRow key={id} login={login} name={name} role={role} />)
-                        }
-                    </div>
-                    <button>Новый админ</button>
+        <AdminsContext.Provider value={admins}>
+            <div className={styles.container}>
+                <NavBar />
+                <section className={styles.adminPage}>
+                    <h1 className={styles.title}>Администраторы</h1>
+                    <div className={styles.currentAdmins}>
+                        <div className={styles.anminsHeader}>
+                            <p>Логин</p>
+                            <p>ФИО</p>
+                            <p>Роль</p>
+                            <div></div>
+                            <div></div>
+                        </div>
+                    <AdminTable />
+                    <button className={styles.btnNew}>Новый админ</button>
                 </div>
             </section>
         </div>
+
+        </AdminsContext.Provider>
     )
 }
