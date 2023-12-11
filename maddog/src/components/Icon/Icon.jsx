5adae51f3b97a3bba5cddd068b80e9480a8ecb6f
Mikhail Sulim <mika-1984@yandex.ru>
2023-12-11 21:56:08 +0300

    style: add component description

--- maddog/src/components/Icon/Icon.jsx
+++ maddog/src/components/Icon/Icon.jsx
@@ -1,5 +1,13 @@
 import React from 'react';
 
+/**
+ * @description Компонент для отображения иконок
+ * Все иконки находятся в спрайте в публичной папке /images/sprite.svg
+ * @param {iconId: string} iconId - id необходимой иконки, можно получить при помощи расширения для VSCode SVG Sprites Viewer 
+ * @returns {JSX.Element} 
+ */
+
+
 const Icon = ({iconId}) => {
   return (
     <svg>
