d128aff7f018657219e929ded006b77a9f5647d9
Sivchenko-Anna <anya.malakhova.99@mail.ru>
2023-12-12 17:23:15 +1000

    add logo and styles for table

--- maddog/src/projectsPages/estimatePage/EstimateTable.jsx
+++ maddog/src/projectsPages/estimatePage/EstimateTable.jsx
@@ -1,45 +1,50 @@
 import styles from './EstimateTable.module.scss'
+import logo from './logoInEstimateTable.png'
 
 export default function EstimateTable() {
     return (
         <div className={styles.tableContainer}>
             <table className={styles.table}>
-                <tr>
-                    <td rowSpan={7}>Логотип</td>
-                    <th rowSpan={2} colSpan={2}>Съемочный период:</th>
-                    <td colSpan={4}>02-10-2023</td>
-                </tr>
-                <tr>
-                    <td colSpan={4}>19-10-2023</td>
-                </tr>
-                <tr>
-                    <th colSpan={3}>Количество смен:</th>
-                    <td colspan={2}><input type="number" value={2}/></td>
-                </tr>
-                <tr>
-                    <th>Проект:</th>
-                    <td colSpan={5}>Название</td>
-                </tr>
-                <tr>
-                    <th>Оператор:</th>
-                    <td colSpan={5}>Оператор: 
-                        <select>
-                            <option>Выберите оператора</option>
-                            <option>Клиент</option>
-                            <option>Менеджер</option>
-                        </select>
-                    </td>
-                </tr>
-                <tr>
-                    <th>Заказчик:</th>
-                    <td colSpan={5}>Иванов Сергей</td>
-                </tr>
-                <tr>
-                    <th>Менеджер:</th>
-                    <td>Petrov Ivan</td>
-                    <td>+79887555454</td>
-                    <td><a href="https://maddogrental.pro/">https://maddogrental.pro/</a></td>
-                </tr>
+                <tbody>
+                    <tr>
+                        <td rowSpan={7} className={styles.tdLogo}>
+                            <img className={styles.img} src={logo} alt='MadDog Rental Logo' />
+                        </td>
+                        <th rowSpan={2}>Съемочный период:</th>
+                        <td colSpan={4}>02-10-2023</td>
+                    </tr>
+                    <tr>
+                        <td colSpan={4}>19-10-2023</td>
+                    </tr>
+                    <tr>
+                        <th>Количество смен:</th>
+                        <td colSpan={3}><input type="text" defaultValue={1}/></td>
+                    </tr>
+                    <tr>
+                        <th>Проект:</th>
+                        <td colSpan={5}>Название</td>
+                    </tr>
+                    <tr>
+                        <th>Оператор:</th>
+                        <td colSpan={5}>
+                            <select>
+                                <option>Выберите оператора</option>
+                                <option>Клиент</option>
+                                <option>Менеджер</option>
+                            </select>
+                        </td>
+                    </tr>
+                    <tr>
+                        <th>Заказчик:</th>
+                        <td colSpan={5}>Иванов Сергей</td>
+                    </tr>
+                    <tr>
+                        <th>Менеджер:</th>
+                        <td>Petrov Ivan</td>
+                        <td>+79887555454</td>
+                        <td><a href="https://maddogrental.pro/">https://maddogrental.pro/</a></td>
+                    </tr>
+                </tbody>
             </table>
         </div>
     )
