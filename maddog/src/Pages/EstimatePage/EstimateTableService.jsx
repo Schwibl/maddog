fcec1548602a312f9daa08f071708dbbf92e5e76
Sivchenko-Anna <anya.malakhova.99@mail.ru>
2023-12-19 00:13:23 +1000

    add functionality for adding and deleting rows

--- maddog/src/Pages/EstimatePage/EstimateTableService.jsx
+++ maddog/src/Pages/EstimatePage/EstimateTableService.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-
+
 import Button from '../../components/button/Button';
 
 import styles from './EstimateTable.module.scss';
@@ -7,71 +7,186 @@ import styles from './EstimateTable.module.scss';
 export default function EstimateTableService() {
   const items = ['Механик', 'Такси', 'Фокуспуллер'];
 
-  const [cost, setCost] = useState('');
-  const [quantity, setQuantity] = useState('');
-  const [days, setDays] = useState('');
+  const [rows, setRows] = useState([
+    {
+      id: 1,
+      cost: '',
+      quantity: '',
+      days: '',
+    },
+  ]);
 
-  const calculateTotal = () => {
+  // Функция для добавления нового ряда
+  const handleAddRow = () => {
+    const newRow = {
+      id: rows.length + 1,
+      cost: '',
+      quantity: '',
+      days: '',
+    };
+
+    setRows([...rows, newRow]);
+  };
+
+  // Функция для удаления ряда по id
+  const handleDeleteRow = (id) => {
+    const updatedRows = rows.filter((row) => row.id !== id);
+
+    setRows(updatedRows);
+  };
+
+  const handleCostChange = (event, id) => {
+    const value = parseInt(event.target.value);
+    if (!isNaN(value)) {
+      const updatedRows = rows.map((row) => {
+        if (row.id === id) {
+          return { ...row, cost: value };
+        }
+        return row;
+      });
+
+      setRows(updatedRows);
+    } else {
+      const updatedRows = rows.map((row) => {
+        if (row.id === id) {
+          return { ...row, cost: '' };
+        }
+        return row;
+      });
+
+      setRows(updatedRows);
+    }
+  };
+
+  const handleQuantityChange = (event, id) => {
+    const value = parseInt(event.target.value);
+    if (!isNaN(value)) {
+      const updatedRows = rows.map((row) => {
+        if (row.id === id) {
+          return { ...row, quantity: value };
+        }
+        return row;
+      });
+
+      setRows(updatedRows);
+    } else {
+      const updatedRows = rows.map((row) => {
+        if (row.id === id) {
+          return { ...row, quantity: '' };
+        }
+        return row;
+      });
+
+      setRows(updatedRows);
+    }
+  };
+
+  const handleDaysChange = (event, id) => {
+    const value = parseInt(event.target.value);
+    if (!isNaN(value)) {
+      const updatedRows = rows.map((row) => {
+        if (row.id === id) {
+          return { ...row, days: value };
+        }
+        return row;
+      });
+
+      setRows(updatedRows);
+    } else {
+      const updatedRows = rows.map((row) => {
+        if (row.id === id) {
+          return { ...row, days: '' };
+        }
+        return row;
+      });
+
+      setRows(updatedRows);
+    }
+  };
+
+  const calculateTotal = (cost, quantity, days) => {
     return Math.ceil(cost * quantity * days);
-  };
-
-  const price = calculateTotal();
-
-  const handleCostChange = (event) => {
-    const value = parseInt(event.target.value);
-    if (!isNaN(value)) {
-      setCost(value);
-    } else {
-      setCost('');
-    }
-  };
-
-  const handleQuantityChange = (event) => {
-    const value = parseInt(event.target.value);
-    if (!isNaN(value)) {
-      setQuantity(value);
-    } else {
-      setQuantity('');
-    }
-  };
-
-  const handleDaysChange = (event) => {
-    const value = parseInt(event.target.value);
-    if (!isNaN(value)) {
-      setDays(value);
-    } else {
-      setDays('');
-    }
   };
 
   return (
     <tbody>
       <tr>
         <td>
-          <Button className={styles.addService} type='button' value='add-service' name='add-service' children='+'/>
+          <Button className={styles.addService}
+            type='button'
+            value='add-service'
+            name='add-service'
+            children='+'
+            onClick={handleAddRow}
+          />
         </td>
         <th colSpan={7}>Обслуживание и транспорт</th>
       </tr>
-      <tr>
-        <td>
-          <Button className={styles.deleteService} type='button' value='delete-service' name='delete-service' children='-'/>
-        </td>
-        <td>
-          <select className={styles.selectService} name='service'>
-            <option value=''>Выберите тип обслуживания</option>
-            {items.map((item) => (
-              <option value={item} key={item} className={styles.optionService}>{item}</option>
-            ))}
-          </select>
-        </td>
-        <td><input type='text' name='cost' value={cost} onChange={handleCostChange} className={styles.input} placeholder='Цена за смену'/></td>
-        <td><input type='text' name='quantity' value={quantity} onChange={handleQuantityChange} className={styles.input} placeholder='Количество'/></td>
-        <td><input type='text' name='days' value={days} onChange={handleDaysChange} className={styles.input} placeholder='Количество'/></td>
-        <td>-</td>
-        <td>{price}</td>
-        <td>-</td>
+      {rows.map((row) => {
+        const { id, cost, quantity, days } = row;
+        const price = calculateTotal(cost, quantity, days);
+
+        return (
+          <tr key={id}>
+            <td>
+              <Button
+                className={styles.deleteService}
+                type='button'
+                value='delete-service'
+                name='delete-service'
+                children='-'
+                onClick={() => handleDeleteRow(id)}
+              />
+            </td>
+            <td>
+              <select className={styles.selectService} name='service'>
+                <option value=''>Выберите тип обслуживания</option>
+                {items.map((item) => (
+                  <option value={item} key={item} className={styles.optionService}>
+                    {item}
+                  </option>
+                ))}
+              </select>
+            </td>
+            <td>
+              <input
+                type='text'
+                name='cost'
+                value={cost}
+                onChange={(event) => handleCostChange(event, id)}
+                className={styles.input}
+                placeholder='Цена за смену'
+              />
+            </td>
+            <td>
+              <input
+                type='text'
+                name='quantity'
+                value={quantity}
+                onChange={(event) => handleQuantityChange(event, id)}
+                className={styles.input}
+                placeholder='Количество'
+              />
+            </td>
+            <td>
+              <input
+                type='text'
+                name='days'
+                value={days}
+                onChange={(event) => handleDaysChange(event, id)}
+                className={styles.input}
+                placeholder='Количество'
+              />
+            </td>
+            <td>-</td>
+            <td>{price}</td>
+            <td>-</td>
+          </tr>
+        );
+      })}
+      <tr colSpan={7}>
+        <br />
       </tr>
-      <tr colSpan={7}><br /></tr>
     </tbody>
   );
 }
