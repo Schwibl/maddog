ecab027b03e9cf721f5cd7e31b67c7d7f2aa6e4b
Marina Moroz <moroz2387moroz@yandex.ru>
2023-12-18 22:25:42 +1000

    migrating to routerProvider

--- maddog/src/App.js
+++ maddog/src/App.js
@@ -1,33 +1,19 @@
 // import logo from './panel-logo2x.webp';
 // import styles from './App.module.scss';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import UserContext from "./context/UserContext";
 import AuthorizePage from './authorizePage/AuthorizePage';
-import ProjectPage from './projectsPages/projectPage/ProjectPage';
-import AdminPage from './adminPage/AdminPage';
-import UserContext from './context/UserContext';
-import ContactsPage from './Pages/ContactsPage/ContactsPage';
 // import CreateContactPage from './Pages/CreateContactPage/CreateContactPage';
 
 function App() {
   const user = {
     id: 123,
-    role: 'admin',
-    contacts: 'example@example.com',
-    name: 'Skelork',
+    role: "admin",
+    contacts: "example@example.com",
+    name: "Skelork",
   };
-  return (
-    <UserContext.Provider value={user}>
-      <BrowserRouter className="App">
-        <Routes>
-          <Route path="/" element={<AuthorizePage />} />
-          <Route path="projects" element={<ProjectPage />} />
-          <Route path="admin" element={<AdminPage />} />
-          <Route path="contacts" element={<ContactsPage />} />
-          {/* <Route path="create" element={<CreateContactPage />} /> */}
-        </Routes>
-      </BrowserRouter>
-    </UserContext.Provider>
-  );
+  return <UserContext.Provider value={user}>
+    <AuthorizePage />
+  </UserContext.Provider>;
 }
 
 export default App;
--- maddog/src/index.js
+++ maddog/src/index.js
@@ -4,12 +4,48 @@ import "./index.scss";
 import App from "./App";
 import { store } from "./store";
 import { Provider } from "react-redux";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import { Layout } from "./layouts/Layout";
+import { NotFound } from "./Pages/404/404";
+import ProjectPage from "./projectsPages/projectPage/ProjectPage";
+import ContactsPage from "./Pages/ContactsPage/ContactsPage";
+import AdminPage from "./adminPage/AdminPage";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    errorElement: <NotFound />,
+    children: [
+      {
+        path: "/",
+        element: <App />,
+      },
+      {
+        path: "/projects",
+        element: <ProjectPage />,
+      },
+      {
+        path: "/admin",
+        element: <AdminPage />,
+      },
+      {
+        path: "/contacts",
+        element: <ContactsPage />,
+      },
+    ],
+  },
+  {
+    path: "*",
+    element: <NotFound />,
+  },
+]);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <RouterProvider router={router} />
     </Provider>
   </React.StrictMode>
 );
