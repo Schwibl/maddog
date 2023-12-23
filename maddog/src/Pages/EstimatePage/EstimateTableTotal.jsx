6e396e25c6304703914466f1dce9c35c37ea6e4d
Sivchenko-Anna <anya.malakhova.99@mail.ru>
2023-12-23 23:27:22 +1000

    update styles, add mixins

--- maddog/src/Pages/EstimatePage/EstimateTableTotal.jsx
+++ maddog/src/Pages/EstimatePage/EstimateTableTotal.jsx
@@ -21,44 +21,44 @@ export default function EstimateTableTotal() {
         <td rowSpan={4} colSpan={4}>
           Итоговая стоимость оборудования
         </td>
-        <td colSpan={3}>В смену</td>
+        <td colSpan={3} className={styles.textLeft}>В смену</td>
         <td rowSpan={4}></td>
       </tr>
       <tr>
-        <td colSpan={3}>
-            Скидка % <input type='text' name='discount' value={discount} onChange={(event) => {handleChange(event, setDiscount);}} className={styles.input} placeholder='Скидка %'/>
+        <td colSpan={3} className={styles.textLeft}>
+            Скидка % <input type='text' name='discount' value={discount} onChange={(event) => {handleChange(event, setDiscount);}} className={styles.inputTotal} placeholder='Скидка %'/>
         </td>
       </tr>
       <tr>
-        <td colSpan={3}>С учетом скидки</td>
+        <td colSpan={3} className={styles.textLeft}>С учетом скидки</td>
       </tr>
       <tr>
-        <td colSpan={3}>За проект</td>
+        <td colSpan={3} className={styles.textLeft}>За проект</td>
       </tr>
       <tr>
         <td rowSpan={2} colSpan={4}>
           Итоговая стоимость обслуживания
         </td>
-        <td colSpan={3}>В смену</td>
+        <td colSpan={3} className={styles.textLeft}>В смену</td>
         <td rowSpan={2}></td>
       </tr>
       <tr>
-        <td colSpan={3}>За проект</td>
+        <td colSpan={3} className={styles.textLeft}>За проект</td>
       </tr>
       <tr>
         <td rowSpan={3} colSpan={4}>
           Общая стоимость
         </td>
-        <td colSpan={3}>За проект</td>
+        <td colSpan={3} className={styles.textLeft}>За проект</td>
         <td rowSpan={3}></td>
       </tr>
       <tr>
-        <td colSpan={3}>
-            Процент УСН <input type='number' name='tax' value={tax} onChange={(event) => {handleChange(event, setTax);}} className={styles.input} placeholder='УСН %'/>
+        <td colSpan={3} className={styles.textLeft}>
+            Процент УСН <input type='number' name='tax' value={tax} onChange={(event) => {handleChange(event, setTax);}} className={styles.inputTotal} placeholder='УСН %'/>
         </td>
       </tr>
       <tr>
-        <td colSpan={3}>При оплате по УСН</td>
+        <td colSpan={3} className={styles.textLeft}>При оплате по УСН</td>
       </tr>
     </tbody>
   );
