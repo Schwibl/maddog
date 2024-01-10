5e17a5149b83e2680312f9df8043adc66275abe6
Mikhail Sulim <mika-1984@yandex.ru>
2024-01-10 23:08:45 +0300

    feat: add ag-grid

--- maddog/src/Pages/ContactsPage/ContactsPage.jsx
+++ maddog/src/Pages/ContactsPage/ContactsPage.jsx
@@ -1,77 +1,157 @@
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { AgGridReact } from 'ag-grid-react';
+import React, { useState, useMemo, useCallback } from 'react';
+import { createPortal } from 'react-dom';
 
 import Button from '../../components/button/Button';
-import Input from '../../components/Input/Input';
+import Icon from '../../components/Icon/Icon';
 import NavBar from '../../components/navbar/NavBar';
-import Select from '../../components/Select/Select';
+import { AG_GRID_LOCALE_RU } from '../../utils/ag-grid-locale-ru';
 
-import ContactRow from './ContactRow';
-import { types, tableHeader, contacts } from './mock';
+import { types, contacts } from './mock';
 
 import styles from './ContactsPage.module.scss';
 
+// eslint-disable-next-line import/order, import/no-unresolved
+import 'ag-grid-community/styles/ag-grid.css';
+
+
 /**
  * @description Страница для отображения всех контактов
- * 
- * @returns {JSX.Element} 
+ *
+ * @returns {JSX.Element}
  */
 
 const ContactsPage = () => {
-  const [filter, setFilter] = useState('');
-  const [selectedType, setSelectedType] = useState();
+  const [isShowModal, setIsShowModal] = useState(false);
+  const [modalUrl, setModalUrl] = useState('');
 
-  const handleFilterChange = (event) => {
-    // обработчик ввода значений в поисковую строку
-    setFilter(event.target.value);
+  const PhotoModal = ({ url }) => {
+    return (
+      <div className={styles.modal}>
+        <img src={url} alt='' />
+      </div>
+    );
   };
 
-  const handleTypeChange = (event) => {
-    // обработчик выбора типа контакта
-    setSelectedType(event.target.selectedIndex);
+  const avatarFormatter = ({ value }) => {
+    return (
+      <img
+        onMouseEnter={
+          () => {
+            setIsShowModal(true);
+            setModalUrl(value);
+          }
+        }
+        onMouseLeave={() => {
+          setIsShowModal(false);
+        }}
+        src={value}
+        alt=''
+      />
+    );
   };
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
+  const editFormatter = (p) => {
+    return (
+      <button
+        type='button'
+        className={styles.tableBtn}
+        onClick={() => {
+          window.alert('value= ', p.data.name);
+          console.log(p.data.name);
+        }}
+      >
+        <Icon iconId='edit' />
+      </button>
+    );
   };
 
+  const gridOptions = {
+    localeText: AG_GRID_LOCALE_RU,
+  };
 
-  const filteredContacts = contacts
-    .filter(({ name }) => name.toLowerCase().includes(filter.toLowerCase()))
-    .filter(({ role }) => selectedType ? role === selectedType : true);
+  const cellClickedListener = useCallback((e) => {
+    console.log(e);
+  });
+
+  const tableHeader = [
+    {
+      headerName: 'ФИО',
+      field: 'name',
+      flex: 6,
+      resizable: true,
+      sortable: true,
+      // filter: true,
+     
+      floatingFilter: true,
+      filter: 'agTextColumnFilter', 
+      cellClass: 'vertical-middle',
+    },
+    {
+      headerName: 'Тип контакта',
+      field: 'role',
+      flex: 2,
+      sortable: true,
+      filter: 'agSetColumnFilter',
+      cellClass: 'all-middle',
+    },
+    {
+      headerName: 'Фото',
+      field: 'photoUrl',
+      sortable: false,
+      flex: 1,
+      filter: false,
+      cellClass: 'vertical-middle',
+      cellRenderer: avatarFormatter,
+    }
+  ];
+
+  const [typesList, setTypesList] = useState(types);
+
+
+  const [columnDefs, setColumnDefs] = useState(tableHeader);
+
+  const [rowData, setRowData] = useState(
+    contacts.map(({ name, role, photoUrl }) => ({
+      name,
+      role: types.find((type) => type.id === role).role,
+      photoUrl,
+    }))
+  );
 
 
   return (
     <main className={styles.main}>
+      {isShowModal && createPortal(<PhotoModal url={modalUrl} />, document.body)}
       <NavBar />
       <section className={styles.contactsPage}>
-        <div className={styles.filterBlock}>
-          <form className={styles.filterForm} onSubmit={handleSubmit}>
-            <Input type='text' placeholder='ФИО' name='name' onChange={handleFilterChange} />
-            <Select items={types}
-              placeholder='Выберите тип контакта'
-              name='role'
-              onChange={handleTypeChange}
-            />
-            <Button type='submit' className={styles.button}>Найти</Button>
-          </form>
+        <h1 className={styles.title}>Контакты</h1>
 
-          <Button className={styles.button} type='button' >
-            <Link to='/create' >
-                            Создать
-            </Link>
-          </Button>
-        </div>
-
-        <div className={styles.table}>
-          <div className={styles.gridRowHeader}>
-            {tableHeader.map((item, idx) => (
-              <div className={styles.gridHeader} key={idx}>{item}</div>
-            ))}
+        <div className={styles.agTable}>
+          <div className={styles.buttons}>
+            <button className={styles.button} type='button'>
+              Удалить
+            </button>
+            <button className={styles.button} type='button'>
+              Редактировать
+            </button>
+            <button className={styles.button} type='button'>
+              Добавить тип контакта
+            </button>
+            <button className={styles.button} type='button'>
+              Добавить контакт
+            </button>
           </div>
-          {filteredContacts.map(({ id, name, photoUrl }) => ({ id, name, photoUrl })).map((item, idx) => (
-            <ContactRow key={idx} {...item} />
-          ))}
+          <div className={styles['ag-table']}>
+            <AgGridReact
+              onCellClicked={cellClickedListener}
+              rowData={rowData}
+              defaultColDef={{ flex: 1 }}
+              pagination={true}
+              columnDefs={columnDefs}
+              gridOptions={gridOptions}
+            />
+          </div>
         </div>
       </section>
     </main>
