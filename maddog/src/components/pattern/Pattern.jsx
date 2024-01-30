ee8c918e493f321bd0b795eab206f29ce66dbfef
Bosikova Liubov Sergeevna <gulidova06@mail.ru>
2024-01-31 00:09:03 +0300

    section component separated, common pattern styles

--- maddog/src/components/pattern/Pattern.jsx
+++ maddog/src/components/pattern/Pattern.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 
+import SectionPattern from './SectionPattern';
 import ToolRow from './ToolRow';
-
 
 import styles from './Pattern.module.scss';
 
@@ -12,35 +12,15 @@ function Pattern(props) {
     <div className={styles.table}>
       {sections
         .filter(({ name }) => name != 'ОБСЛУЖИВАНИЕ')
-        .map((toolSection) => {
-          return (
-            <div className={styles.sectionWrap} id={toolSection.id}>
-              <h2 className={styles.sectionName}>
-                {toolSection.name} <span className={styles.deleteSection}>Удалить секцию</span>
-              </h2>
-              <div className={styles.sectionHeader}>
-                <p className={styles.headerDelete}>Удалить</p>
-                <p>Название</p>
-                <p>Количество</p>
-              </div>
-              <div className={styles.toolsWrap}>
-                {toolSection.tools.map((tool) => (
-                  <ToolRow
-                    key={tool.id}
-                    id={tool.id}
-                    name={tool.name}
-                    quantity={tool.quantity}
-                    sections={sections}
-                    setSections={setSections}
-                  ></ToolRow>
-                ))}
-              </div>
-              <div className={styles.addToolWrap}>
-                <button className={styles.addTool}>+ Добавить оборудование</button>
-              </div>
-            </div>
-          );
-        })}
+        .map((toolSection) => (
+          <SectionPattern
+            id={toolSection.id}
+            name={toolSection.name}
+            tools={toolSection.tools}
+            sections={sections}
+            setSections={setSections}
+          ></SectionPattern>
+        ))}
 
       <h2>Обслуживание</h2>
     </div>
--- maddog/src/components/pattern/ToolRow.jsx
+++ maddog/src/components/pattern/ToolRow.jsx
@@ -1,10 +1,14 @@
-import styles from './ToolRow.module.scss';
+import styles from './Pattern.module.scss';
 
 function ToolRow(props) {
   const { id, name, quantity, sections, setSections } = props;
+
+  function handleDeleteTool () {
+
+  }
   return (
     <div className={styles.tool} id={id}>
-      <button className={styles.toolDelete}>X</button>
+      <button className={styles.toolDelete} onClick={handleDeleteTool}>X</button>
       <input className={styles.toolName} name='name' type='text' value={name}></input>
       <input className={styles.toolQuantity} name='quantity' type='number' value={quantity} />
     </div>
